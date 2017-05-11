135c303bdd4e8a24c201686b08cf24e0fcb277ad
Aaron Smith <aaronsmith.dev@gmail.com>
2017-05-11 10:59:22 -0400

    Remove correct files

--- build.ps1
+++ build.ps1
@@ -1,11 +1,3 @@
-param (
-    [switch]$setup = $false
-)
-
-if (!($setup)) {
-    return
-}
-
 If (Test-Path .\Assets\packages) {
     Remove-Item -Recurse -Force .\Assets\packages
 }
@@ -20,15 +12,14 @@ $badDlls = @(
     ".\Assets\packages\DevZH.FlatBuffers.1.4.0\lib\netstandard1.1",
     ".\Assets\packages\DevZH.FlatBuffers.1.4.0\lib\netstandard1.0",
 
-    ".\Assets\packages\NetMQ.4.0.0.1\lib\monoandroid60",
-    ".\Assets\packages\NetMQ.4.0.0.1\lib\net40",
-    ".\Assets\packages\NetMQ.4.0.0.1\lib\netstandard1.3",
-    ".\Assets\packages\NetMQ.4.0.0.1\lib\xamarinios10",
+    ".\Assets\packages\NetMQ.4.0.0.4-rc4\lib\monoandroid60",
+    ".\Assets\packages\NetMQ.4.0.0.4-rc4\lib\net40",
+    ".\Assets\packages\NetMQ.4.0.0.4-rc4\lib\netstandard1.3",
+    ".\Assets\packages\NetMQ.4.0.0.4-rc4\lib\xamarinios10",
 
-    ".\Assets\packages\AsyncIO.0.1.26.0\lib\monoandroid60",
-    ".\Assets\packages\AsyncIO.0.1.26.0\lib\net40",
-    ".\Assets\packages\AsyncIO.0.1.26.0\lib\netstandard1.3",
-    ".\Assets\packages\AsyncIO.0.1.26.0\lib\xamarinios10"
+    ".\Assets\packages\AsyncIO.0.1.23.0\lib\net40",
+    ".\Assets\packages\AsyncIO.0.1.23.0\lib\netstandard1.3",
+    ".\Assets\packages\AsyncIO.0.1.23.0\lib\xamarinios10"
 )
 
 foreach ($dll in $badDlls) {
